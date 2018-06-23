f7c77bb87056661dc2191c11ee8d42130bc60c72
Alexis Montoya <agl125@hotmail.com>
2018-06-23 14:22:41 -0500

    [1] debug status

--- app.js
+++ app.js
@@ -240,7 +240,7 @@ function parseBody (body, callback) {
         match_obj.group = match_row.find('.fi__info__group').first().text().trim();
         match_obj.status = match_row.find('span.period:not(.hidden)').first().text().trim() || 'Pendiente';
 
-        console.log(match_row);
+        console.log(match_row.find('span.period:not(.hidden)'));
 
         let team_home = match_row.find('.fi-t.home').first();
         match_obj.team_home = team_home.find('.fi-t__nText').first().text().trim();
