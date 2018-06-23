d26386a22aa09f9090e4d821c108b1458f668bb7
Alexis Montoya <agl125@hotmail.com>
2018-06-23 14:27:07 -0500

    [1] show in progress on live matches

--- app.js
+++ app.js
@@ -138,8 +138,10 @@ app.post('/find', async (req, res) => {
           let time = moment(match.date + ' ' + match.time, 'YYYYMMDD HH:mm');
           let now = moment();
           if(time.isValid()) {
-            if(moment.duration(time.diff(now))._data.seconds < 0)
+            if(moment.duration(time.diff(now))._data.seconds < 0) {
               match.in_about = 'EN VIVO';
+              match.status = 'En progreso'
+            }
             else
               match.in_about = 'en ' + moment.duration(time.diff(now)).locale('es').humanize();
           } else {
