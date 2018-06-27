a63028d09ad95256cee05e455043ff7a2b01065f
Alexis R. M <agl125@hotmail.com>
2018-06-27 18:24:21 -0500

    [1] returning video or match url if missing

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ app.post('/find', async (req, res) => {
     try {
       let matches = await findMatches(req.body.query || {}, req.body.limit || 0, req.body.sort || {});
       for(match of matches) {
+        match.video = match.video ? match.video : match.match_url;
         match.in_about = ' ';
         if(match.status != 'Final del partido') {
           let time = moment(match.date + ' ' + match.time, 'YYYYMMDD HH:mm');
