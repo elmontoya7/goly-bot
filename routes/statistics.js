c5db96165fce0a102237568f25058545430da508
Alexis Montoya <agl125@hotmail.com>
2018-07-11 10:21:05 -0500

    [1] statistics endponints added

---
+++ routes/statistics.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var types = Object.freeze({
+  goals: 'https://es.fifa.com/worldcup/statistics/players/goal-scored',
+  saves: 'https://es.fifa.com/worldcup/statistics/players/saves',
+  shots: 'https://es.fifa.com/worldcup/statistics/players/shots',
+  cards: 'https://es.fifa.com/worldcup/statistics/players/disciplinary'
+});
+
+router.get('/update', (req, res) => {
+
+});
+
+var updateData = type => {
+  return new Promise((resolve, reject) => {
+    request.get({url: types[type], json:true}, (err, http, body) => {
+      ir(err || !body) return resolve(null);
+      
+    });
+  });
+};
+
+module.exports = router;
